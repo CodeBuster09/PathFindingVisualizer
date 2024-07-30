287e76262f605f04dcb0b270b5b7803910dec436
Shreyansh <sjaiswal0902@gmail.com>
2024-07-30 15:55:54 +0530

    adds the code for dijsktra algorithm

--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -1,25 +1,34 @@
-import React, { Component } from "react"
-import './Node.css'
+import React, {Component} from 'react';
+
+import './Node.css';
 
 export default class Node extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-        };
-    }
+  render() {
+    const {
+      col,
+      isFinish,
+      isStart,
+      isWall,
+      onMouseDown,
+      onMouseEnter,
+      onMouseUp,
+      row,
+    } = this.props;
+    const extraClassName = isFinish
+      ? 'node-finish'
+      : isStart
+      ? 'node-start'
+      : isWall
+      ? 'node-wall'
+      : '';
 
-    render(){
-        const {isEnd, isStart} = this.props;
-        const extraClassname = isEnd
-        ? 'node-finish'
-        : isStart
-        ? 'node-start'
-        : '';
-        return (<div className={`node ${extraClassname}`}></div>)
-    }
+    return (
+      <div
+        id={`node-${row}-${col}`}
+        className={`node ${extraClassName}`}
+        onMouseDown={() => onMouseDown(row, col)}
+        onMouseEnter={() => onMouseEnter(row, col)}
+        onMouseUp={() => onMouseUp()}></div>
+    );
+  }
 }
-
-export const DEFAULT_NODE = {
-    row: 0,
-    col: 0
-};
--- src/components/PathFinder/PathFindingVisualizer.jsx
+++ src/components/PathFinder/PathFindingVisualizer.jsx
@@ -1,53 +1,148 @@
 import React, { Component } from 'react';
 import './PathFindingVisualizer.css';
 import Node from '../Node/Node';
+import { getNodesInShortestPathOrder, dijsktra } from '../../algorithms/dijsktra';
+
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
 
 export default class PathFindingVisualizer extends Component {
-    constructor(props){
-        super(props);
+    constructor(){
+        super();
         this.state = {
-            nodes: [],
+            grid: [],
+            mouseIsPressed: false
         };
     }
 
     componentDidMount() {
-        const nodes = [];
-        for (let row=0; row<20; row++) {
-            const currentRow = [];
-            for(let col=0; col<50; col++) {
-                const currNode = {
-                    col,
-                    row,
-                    isStart: row === 10 && col === 5,
-                    isEnd: row === 10 && col === 45
-                } 
-                currentRow.push(currNode)
-            }
-            nodes.push(currentRow);
+        const grid = getInitialGrid();
+        this.setState({grid})
+    }
+
+    handleMouseDown(row, col) {
+        const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+        this.setState({grid: newGrid, mouseIsPressed: true});
+    }
+
+    handleMouseEnter(row, col) {
+        if (!this.state.mouseIsPressed) return;
+        const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+        this.setState({grid: newGrid});
+    }
+
+    handleMouseUp() {
+        this.setState({mouseIsPressed: false});
+    }
+
+    animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+        for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+          if (i === visitedNodesInOrder.length) {
+            setTimeout(() => {
+              this.animateShortestPath(nodesInShortestPathOrder);
+            }, 10 * i);
+            return;
+          }
+          setTimeout(() => {
+            const node = visitedNodesInOrder[i];
+            document.getElementById(`node-${node.row}-${node.col}`).className =
+              'node node-visited';
+          }, 10 * i);
         }
-        this.setState({nodes})
+      }
+
+      animateShortestPath(nodesInShortestPathOrder) {
+        for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+          setTimeout(() => {
+            const node = nodesInShortestPathOrder[i];
+            document.getElementById(`node-${node.row}-${node.col}`).className =
+              'node node-shortest-path';
+          }, 50 * i);
+        }
+      }
+
+    visualizeDijkstra() {
+        const {grid} = this.state;
+        const startNode = grid[START_NODE_ROW][START_NODE_COL];
+        const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+
+        const visitedNodesInOrder = dijsktra(grid,startNode,finishNode);
+        const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+        this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
     }
 
     render() {
-        const {nodes} = this.state;
+        const {grid, mouseIsPressed} = this.state;
 
         return (
+            <>
+            <button onClick={() => this.visualizeDijkstra()}>
+                Visualize Dijsktra Algorithm
+            </button>
             <div className="grid">
-                {nodes.map((row,rowIdx) => {
+                {grid.map((row,rowIdx) => {
                     
                     return <div key={rowIdx}>
                         {row.map((node,nodeIdx) => {
-                            const {isStart, isEnd} = node;
-                            return <Node key={nodeIdx}
+                            const {row, col, isEnd, isStart, isWall} = node;
+                            return <Node
+                            key={nodeIdx}
+                            col={col}
+                            isFinish={isEnd}
                             isStart={isStart}
-                            isEnd={isEnd} 
-                            test={'foo'} 
-                            >
-                            </Node>})}
+                            isWall={isWall}
+                            mouseIsPressed={mouseIsPressed}
+                            onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                            onMouseEnter={(row, col) =>
+                              this.handleMouseEnter(row, col)
+                            }
+                            onMouseUp={() => this.handleMouseUp()}
+                            row={row}>
+                            </Node>
+                            })}
                     </div>
                 })}
             </div> 
+            </>
         )
 
     }
-}
+};
+
+const getInitialGrid = () => {
+    const grid = [];
+    for (let row = 0; row < 20; row++) {
+      const currentRow = [];
+      for (let col = 0; col < 50; col++) {
+        currentRow.push(createNode(col, row));
+      }
+      grid.push(currentRow);
+    }
+    return grid;
+  };
+
+  const createNode = (col, row) => {
+    return {
+      col,
+      row,
+      isStart: row === START_NODE_ROW && col === START_NODE_COL,
+      isEnd: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+      distance: Infinity,
+      isVisited: false,
+      isWall: false,
+      previousNode: null,
+    };
+  };
+
+  const getNewGridWithWallToggled = (grid, row, col) => {
+    const newGrid = grid.slice();
+    const node = newGrid[row][col];
+    const newNode = {
+      ...node,
+      isWall: !node.isWall,
+    };
+    newGrid[row][col] = newNode;
+    return newGrid;
+  };
