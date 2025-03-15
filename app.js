e0ec3b92b57e00e68de3b1fec2178980295d4bc3
Shreyansh <sjaiswal0902@gmail.com>
2025-03-15 20:59:25 +0530

    Maze generation algorithm implemented

--- app.js
+++ app.js
@@ -179,3 +179,111 @@ const clearWall = ()=> {
         cell.classList.remove('wall');
     })
 }
+
+
+
+
+// MAZE GENERATION
+
+function generateMaze(rowStart, rowEnd, colStart, colEnd, surroundingWall, orientation) {
+
+    //BASE CASE
+    if(rowStart>rowEnd || colStart>colEnd) {
+        return;
+    }
+
+    // STEP 1: CREATE SURROUNDING WALL
+    if(!surroundingWall) {
+        for(let i=0;i<col;i++) {
+            if(!matrix[0][i].classList.contains('source') && !matrix[0][i].classList.contains('target'))
+            matrix[0][i].classList.add('wall');
+
+            if(!matrix[row-1][i].classList.contains('source') && !matrix[row-1][i].classList.contains('target'))
+            matrix[row-1][i].classList.add('wall');
+        }
+
+        for(let i=0;i<row;i++) {
+            if(!matrix[i][0].classList.contains('source') && !matrix[i][0].classList.contains('target'))
+            matrix[i][0].classList.add('wall');
+
+            if(!matrix[i][col-1].classList.contains('source') && !matrix[i][col-1].classList.contains('target'))
+            matrix[i][col-1].classList.add('wall');
+        }
+
+        surroundingWall = true;
+    }
+
+
+    //STEP 2: DRAW WALL VERTICALLY OR HORIZONTALLY
+    if(orientation === 'horizontal') {
+        let possibleRows = [];
+        for(let i=rowStart; i<=rowEnd;i+=2) {
+            possibleRows.push(i);
+        }
+
+        let possibleCols = [];
+        for(let i=colStart-1;i<=colEnd+1;i+=2) {
+            if(i>0 && i<col-1) {
+                possibleCols.push(i);
+            }
+        }
+
+        const currentRow = possibleRows[Math.floor(Math.random() * possibleRows.length)];
+        const randomCol = possibleCols[Math.floor(Math.random() * possibleCols.length)];
+
+        for(let i=colStart-1;i<=colEnd+1;i++) {
+            const cell = matrix[currentRow][i];
+            if(!cell || i === randomCol || cell.classList.contains('source') || cell.classList.contains('target'))
+                continue;
+            cell.classList.add('wall');
+        }
+
+        //Recursive call
+
+        //Upper division
+        generateMaze(rowStart, currentRow-2,colStart,colEnd,surroundingWall,((currentRow-2)-rowStart > colEnd-colStart)?'horizontal':'vertical');
+
+        //Lower division
+        generateMaze(currentRow+2,rowEnd,colStart,colEnd,surroundingWall,(rowEnd-(currentRow+2) > colEnd-colStart)?'horizontal':'vertical');
+
+    } else {
+
+        let possibleCols = [];
+        for(let i=colStart; i<=colEnd;i+=2) {
+            possibleCols.push(i);
+        }
+
+        let possibleRows = [];
+        for(let i=rowStart-1;i<=rowEnd+1;i+=2) {
+            if(i>0 && i<row-1) {
+                possibleRows.push(i);
+            }
+        }
+
+        const currentCol = possibleCols[Math.floor(Math.random() * possibleCols.length)];
+        const randomRow = possibleRows[Math.floor(Math.random() * possibleRows.length)];
+
+        for(let i=rowStart-1;i<=rowEnd+1;i++) {
+            if(!matrix[i]) continue;
+
+            const cell = matrix[i][currentCol];
+            if(i === randomRow || cell.classList.contains('source') || cell.classList.contains('target'))
+                continue;
+
+            cell.classList.add('wall');
+        }
+
+        //Recursive call
+
+        //Left division
+        generateMaze(rowStart, rowEnd, colStart, currentCol-2, surroundingWall, (rowEnd-rowStart > (currentCol-2)-colStart)?'horizontal':'vertical');
+
+        //Right division
+        generateMaze(rowStart, rowEnd, currentCol+2, colEnd, surroundingWall, (rowEnd-rowStart > colEnd-(currentCol+2))?'horizontal':'vertical');
+
+    }
+
+}
+
+
+generateMaze(0,row-1,0,col-1,false,'horizontal');
